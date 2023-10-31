5eef83dcad0ea3ec5fc2318a4f0fd39899865967
felipe-ra <felipeangel50@gmail.com>
2023-10-30 21:42:05 -0500

    added script to display tensors info

---
+++ analyze_training_tensors.py
@@ -0,0 +1,18 @@
+import torch
+
+# Load the tensors from disk
+x_tensor = torch.load('X_tensor.pth')
+y_tensor = torch.load('y_tensor.pth')
+
+# Print the shapes and data types of the tensors
+print("Shape of X_tensor:", x_tensor.shape)
+print("Data type of X_tensor:", x_tensor.dtype)
+print("Number of elements in X_tensor:", torch.numel(x_tensor))
+
+print("Shape of y_tensor:", y_tensor.shape)
+print("Data type of y_tensor:", y_tensor.dtype)
+print("Number of elements in y_tensor:", torch.numel(y_tensor))
+
+# Calculate the size in bytes
+print("Size of X_tensor in bytes:", x_tensor.element_size() * x_tensor.nelement())
+print("Size of y_tensor in bytes:", y_tensor.element_size() * y_tensor.nelement())
