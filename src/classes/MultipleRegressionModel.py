aa436934049e8d448064127222d6a5949b159cdf
felipe-ra <felipeangel50@gmail.com>
2023-10-31 13:05:56 -0500

    added kwargs compatibility

--- src/classes/MultipleRegressionModel.py
+++ src/classes/MultipleRegressionModel.py
@@ -2,8 +2,8 @@ from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.linear_model import LinearRegression
 
 class MultipleRegressionModel(BaseEstimator, RegressorMixin):
-    def __init__(self):
-        self.model = LinearRegression()
+    def __init__(self, **hyperparameters):
+        self.model = LinearRegression(**hyperparameters)
         
     def fit(self, X, y):
         self.model.fit(X, y)
