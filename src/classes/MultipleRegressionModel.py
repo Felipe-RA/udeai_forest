b126e085af77d858e91e9a9b4f22ad0e8917ba5b
felipe-ra <felipeangel50@gmail.com>
2023-11-03 15:19:46 -0500

    added MRegressionModel documentation and comp. layer

--- src/classes/MultipleRegressionModel.py
+++ src/classes/MultipleRegressionModel.py
@@ -2,8 +2,27 @@ from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.linear_model import LinearRegression
 
 class MultipleRegressionModel(BaseEstimator, RegressorMixin):
-    def __init__(self, **hyperparameters):
-        self.model = LinearRegression(**hyperparameters)
+    def __init__(self, fit_intercept=True, copy_X=True, positive=True):
+        """
+        Hyperparameters:
+        - fit_intercept: bool, default=True
+            Whether to calculate the intercept for this model.
+        - copy_X: bool, default=True
+            If True, X will be copied; else, it may be overwritten.
+        - positive: bool, default=True
+            When set to True, forces the coefficients to be positive.
+        """
+        
+        self.fit_intercept = fit_intercept
+        self.copy_X = copy_X
+        self.positive = positive
+        
+        self.model = LinearRegression(
+            fit_intercept=self.fit_intercept, 
+            copy_X=self.copy_X,
+            n_jobs=-1,  # Always use the maximum number of jobs
+            positive=self.positive
+        )
         
     def fit(self, X, y):
         self.model.fit(X, y)
