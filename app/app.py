1ee66366279a9588aa1b66bd557ffb36c559980b
felipe-ra <felipeangel50@gmail.com>
2024-05-19 22:59:59 -0500

    updated backend

--- app/app.py
+++ app/app.py
@@ -28,7 +28,7 @@ async def generate_map(params: tuple = Depends(common_parameters)):
     if not data_dir or not path_to_model:
         raise HTTPException(status_code=400, detail="Missing data directory or model path")
     
-    data_dir, path_to_model = (f"../{data_dir}", f"../{path_to_model}")
+    data_dir, path_to_model = (f"./{data_dir}", f"./{path_to_model}")
 
     # Check if paths are valid
     if not os.path.exists(data_dir):
@@ -39,23 +39,40 @@ async def generate_map(params: tuple = Depends(common_parameters)):
         raise HTTPException(status_code=404, detail=f"Model path not found: {path_to_model}")
 
     # Construct the command to run the Python script
-    command = f"/bin/python3 ../generate_vegetation_map.py {data_dir} {path_to_model}"
+    command = f"/app/venv/bin/python ./generate_vegetation_map.py {data_dir} {path_to_model}"
     logger.debug(f"Running command: {command}")
     try:
         subprocess.run(command, check=True, shell=True)
         map_directory = f'html_maps/{os.path.basename(data_dir)}'
-        return {"message": f"Map generation initiated successfully. Check {map_directory}/vegetation_map.html for the output."}
+        return HTMLResponse(content=f"""
+            <html>
+                <head>
+                    <title>Map Generation</title>
+                </head>
+                <body>
+                    <h1>Map generation initiated successfully.</h1>
+                    <p>Check <a href="/view-map/{os.path.basename(data_dir)}">{map_directory}/vegetation_map.html</a> for the output.</p>
+                    <p style="color: red;">Redirection in 5 seconds or <a href="/view-map/{os.path.basename(data_dir)}">click here</a> to be redirected.</p>
+                    <script>
+                        setTimeout(function() {{
+                            window.location.href = "/view-map/{os.path.basename(data_dir)}";
+                        }}, 5000);
+                    </script>
+                </body>
+            </html>
+        """, status_code=200)
     except subprocess.CalledProcessError as e:
         logger.error(f"Command failed: {e}")
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/view-map/{data_dir}", response_class=HTMLResponse)
 async def view_map(data_dir: str):
-    map_path = f"../html_maps/{data_dir}/vegetation_map.html"
+    map_path = f"html_maps/{data_dir}/vegetation_map.html"
     if os.path.exists(map_path):
         return FileResponse(map_path, media_type='text/html')
     else:
         raise HTTPException(status_code=404, detail="Map not found at "+ map_path)
+
 
 @app.get("/form", response_class=HTMLResponse)
 async def form():
@@ -63,14 +80,23 @@ async def form():
     <html>
         <head>
             <title>Generate Map</title>
+            <script>
+                function toggleLock() {
+                    var modelPathInput = document.getElementById("path_to_model");
+                    var lockCheckbox = document.getElementById("lock_model_path");
+                    modelPathInput.readOnly = !lockCheckbox.checked;
+                }
+            </script>
         </head>
         <body>
             <h1>Generate Map</h1>
             <form id="generate-map-form" action="/generate-map/" method="post">
                 <label for="data_dir">Data Directory:</label>
-                <input type="text" id="data_dir" name="data_dir"><br><br>
+                <input type="text" id="data_dir" name="data_dir" required><br><br>
                 <label for="path_to_model">Model Path:</label>
-                <input type="text" id="path_to_model" name="path_to_model"><br><br>
+                <input type="text" id="path_to_model" name="path_to_model" value="src/trained_models/VGGUdeaSpectral1/VGGUdeaSpectral_model1.joblib" readonly required>
+                <input type="checkbox" id="lock_model_path" checked onclick="toggleLock()"> Lock
+                <br><br>
                 <input type="submit" value="Submit">
             </form>
             <div id="loading-message" style="display:none;">
